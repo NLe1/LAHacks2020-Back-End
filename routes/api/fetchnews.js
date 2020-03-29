852f35d5a6a4e1fac480d54ac058d3f81b8a5ce2
Brandon Liang <54skyxenon@gmail.com>
2020-03-29 01:39:09 -0700

    added pagination for top news headlines for a certain topic

--- routes/api/fetchnews.js
+++ routes/api/fetchnews.js
@@ -5,9 +5,8 @@ const config = require("config");
 const router = express.Router();
 var query = 'test';
 
-//Set up
+// Set up
 const newsapi = new NewsAPI(config.get("newsapi.APIKey"));
-
 
 /* @GET  api/news/top
 // @desc get oauth bearer token
@@ -22,6 +21,30 @@ router.get("/top", (req, res) => {
     country: 'us'
   }).then(response => {
     res.send(response)
+  }).catch(err => {
+    res.send(err)
+  });
+});
+
+/* @GET  api/news/results
+// @desc get oauth bearer token
+// @access Private
+*/
+router.get("/results", (req, res) => {
+  const { topic, pageNumber, paginationLength } = req.body;
+
+  // en for language, us for country, and the user input for q, rest can be blank
+  newsapi.v2.topHeadlines({
+    sources: '',
+    q: topic,
+    category: '',
+    language: 'en',
+    country: 'us'
+  }).then(response => {
+    const start = (pageNumber - 1) * paginationLength;
+    const end = start + paginationLength;
+    const results = response.articles.slice(start, end);
+    res.send(results);
   }).catch(err => {
     res.send(err)
   });
